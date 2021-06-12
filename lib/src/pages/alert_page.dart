a260c89617efd34be3eb3872f2fbb29ab12ba52c
Samuel De La Hoz <samueldlh.dev@gmail.com>
2021-06-12 16:47:12 -0500

    alert, avatar page

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -4,9 +4,67 @@ class AlertPage extends StatelessWidget{
 
    @override
    Widget build(BuildContext context) {
-     return Scaffold(
-        appBar: AppBar(title: Text('Alert')),
+      return Scaffold(
+         appBar: AppBar(title: Text('Alert')),
+         body: Center(
+            child: ElevatedButton(
+               child: Text('MOSTRAR ALERTA'),
+               onPressed: () => _alert(context),
+               style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  shape: StadiumBorder()
+               ),
+            ),
+         ),
+         floatingActionButton: FloatingActionButton(
+           child: Icon( Icons.mode_standby_rounded),
+           onPressed: () {
+
+           },
+         ),
      );
+   }
+
+   void _alert(BuildContext context){
+      showDialog(
+         context: context, 
+         barrierDismissible: false,
+         builder: (context) {
+            return AlertDialog(
+               title: Text('TITULO'),
+               shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(19.0) ),
+               content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                     Text('Este es el contenido de la caja de la alerta'),
+                     Image(
+                        image: AssetImage('assets/app.png'),
+                        height: 99.0,
+                        fit: BoxFit.cover,
+                     ),
+                  ],
+               ),
+               actions: [
+                  ElevatedButton(
+                     child: Text('cancelar'),
+                     onPressed: () => Navigator.of(context).pop(),
+                     style: ElevatedButton.styleFrom(
+                        primary: Colors.purple,
+                        shape: StadiumBorder()
+                     ),
+                  ),
+                  ElevatedButton(
+                     child: Text('ok'),
+                     onPressed: () => Navigator.of(context).pop(),
+                     style: ElevatedButton.styleFrom(
+                        primary: Colors.purple,
+                        shape: StadiumBorder()
+                     ),
+                  ),
+               ],
+            );
+         }
+      );
    }
 
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -4,14 +4,39 @@ class AvatarPage extends StatelessWidget {
   	@override
   	Widget build(BuildContext context) {
     	return  Scaffold(
-        appBar: AppBar(
-          title: Text('avatar'),
+         appBar: AppBar(
+            title: Text('avatar'),
+            actions: [
+
+               Container(
+                  margin: EdgeInsets.only(right: 9.0),
+                  child: CircleAvatar(
+                     child: CircleAvatar(
+                        backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBftyUAo76cg-Litu-LJAm1sfigvMq2ZwDBw&usqp=CAU'),
+                     ),
+                 ),
+               ),
+
+               Container(
+                  margin: EdgeInsets.only(right: 9.0),
+                  child: CircleAvatar(
+                     child: CircleAvatar(
+                        child: Text('RICK'),
+                        backgroundColor: Colors.black,
+                     ),
+                 ),
+               )
+            
+            ],
         	),
         	body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+                  child: Container(
+                  child: FadeInImage(
+                     image: NetworkImage('https://p4.wallpaperbetter.com/wallpaper/410/59/609/rick-and-morty-tv-rick-sanchez-morty-smith-wallpaper-preview.jpg'),
+                     placeholder: AssetImage('assets/jar-loading.gif'),
+                  ),
+               ),
+            ),
       );
   }
 }
